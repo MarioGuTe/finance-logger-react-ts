d08d5212342e020d5271ea62ecdd751cd0a61978
MarioGuTe <mariogutierreztello@gmail.com>
2023-10-07 20:55:38 -0300

    pass updateLogInfo function as Props

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,13 +1,25 @@
+import { useState } from "react";
 import Form from "../atoms/Form/Form";
+import { INewLog } from "../interfaces/INewLog";
 
 function App() {
+  const [newLog, setNewLog] = useState<INewLog[]>([]);
+
+  function updateLogInfo(formData: INewLog) {
+    return setNewLog((currentData) => {
+      return [...currentData, formData];
+    });
+  }
+
+  console.log(newLog);
+
   return (
     <>
       <div className="wrapper">
         <h1>Finance Logger</h1>
       </div>
       <footer>
-        <Form />
+        <Form formData={updateLogInfo} />
       </footer>
     </>
   );
