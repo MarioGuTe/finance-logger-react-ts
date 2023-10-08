1409d935d33599b0475e44c9dbb63f1a50401452
MarioGuTe <mariogutierreztello@gmail.com>
2023-10-08 02:02:50 -0300

    add List, deleteLog Function and List props

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import List from "../atoms/List/List";
 import Form from "../atoms/Form/Form";
 import { INewLog } from "../interfaces/INewLog";
 
@@ -11,12 +12,17 @@ function App() {
     });
   }
 
-  console.log(newLog);
+  function deleteLog(id: string) {
+    setNewLog((currentData) => {
+      return currentData.filter((newLog) => newLog.id !== id);
+    });
+  }
 
   return (
     <>
       <div className="wrapper">
         <h1>Finance Logger</h1>
+        <List logInfo={newLog} onItemClick={deleteLog} />
       </div>
       <footer>
         <Form formData={updateLogInfo} />
