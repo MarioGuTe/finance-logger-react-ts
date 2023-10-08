89489c39251b45dfb355ce7659a083f95b7304e0
MarioGuTe <mariogutierreztello@gmail.com>
2023-10-07 21:09:29 -0300

    add formData as props

--- src/atoms/Form/Form.tsx
+++ src/atoms/Form/Form.tsx
@@ -1,28 +1,51 @@
 import { useState } from "react";
+import { INewLog } from "../../interfaces/INewLog";
 
-const Form = () => {
-  let defaultFormInfo = {
+interface Props {
+  formData?: (params: INewLog) => void;
+}
+
+const Form = ({ formData }: Props) => {
+  const defaultFormInfo = {
     id: self.crypto.randomUUID(),
-    // finance_type: "invoice",
+    finance_type: "invoice",
     to_from: "",
     details: "",
-    // amount: "",
+    amount: "",
   };
 
   const [formInfo, setFormInfo] = useState(defaultFormInfo);
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    formData && formData(formInfo);
+    setFormInfo(defaultFormInfo);
+  };
+
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLSelectElement>
+  ): void => {
     const value = e.target.value;
     setFormInfo({ ...formInfo, [e.target.name]: value });
-    console.log(formInfo);
   };
 
   return (
     <>
-      <form
-        className="new-item-form"
-        //    onSubmit={handleSubmit}
-      >
+      <form className="new-item-form" onSubmit={handleSubmit}>
+        <div className="field">
+          <label>Type:</label>
+          <select
+            id="type"
+            name="finance_type"
+            onChange={handleChange}
+            value={formInfo.finance_type}
+          >
+            <option value="invoice">Invoice</option>
+            <option value="payment">Payment</option>
+          </select>
+        </div>
         <div className="field">
           <label>To / From:</label>
           <input
@@ -41,6 +64,17 @@ const Form = () => {
             id="details"
             name="details"
             value={formInfo.details}
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div className="field">
+          <label>Amount ($):</label>
+          <input
+            type="number"
+            id="amount"
+            name="amount"
+            value={formInfo.amount}
             onChange={handleChange}
             required
           />
